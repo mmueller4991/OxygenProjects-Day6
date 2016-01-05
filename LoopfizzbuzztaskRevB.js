15293e4a495e9473ca12ba86acff513d8be539b4
mmueller4991 <mmueller4991@gmail.com>
2016-01-05 13:39:41 -0600

    Add minor changes to RevB FizzBuzz js file

---
+++ LoopfizzbuzztaskRevB.js
@@ -0,0 +1,13 @@
+"use strict":
+
+for (var count=0; count <= 100; count++) {
+
+	if (count % 15 === 0)
+		console.log("FizzBuzz");
+	else if (count % 3 === 0)
+		console.log("Fizz");
+	else if (count % 5 === 0)
+		console.log("Buzz");
+	else
+		console.log(count);
+}
