8af9ab5333f3e10843d9ba87205b115c5bdf0a31
mmueller4991 <mmueller4991@gmail.com>
2016-01-05 09:10:08 -0600

    Replaced variable name from Harry to Count

--- LoopfizzbuzztaskRevA.js
+++ LoopfizzbuzztaskRevA.js
@@ -1,13 +1,13 @@
 "use strict";
 
-for (var harry=1; harry <= 100; harry++)
+for (var count=1; count <= 100; count++)
 	{
-    if (harry % 15 == 0)
+    if (count % 15 === 0)
         console.log("FizzBuzz");
-    else if (harry % 3 == 0)
+    else if (count % 3 === 0)
         console.log("Fizz");
-    else if (harry % 5 == 0)
+    else if (count % 5 === 0)
         console.log("Buzz");
     else
-        console.log(harry);
+        console.log(count);
 }
